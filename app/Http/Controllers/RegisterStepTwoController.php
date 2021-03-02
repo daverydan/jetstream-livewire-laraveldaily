7e4f799f555dd62d1092c31ef01a460d7d47af23
Danny Avery <danny.w.avery@gmail.com>
2021-03-02 18:51:43 -0500

    duplicate profile photoUpload w/ Jetstream in Step 2 Reg.

--- app/Http/Controllers/RegisterStepTwoController.php
+++ app/Http/Controllers/RegisterStepTwoController.php
@@ -21,6 +21,10 @@ class RegisterStepTwoController extends Controller
             'city_id' => request()->city_id,
         ]);
 
+        if (request()->hasFile('photo')) {
+            auth()->user()->updateProfilePhoto(request()->photo);
+        }
+
         return redirect()->route('dashboard');
     }
 }
--- resources/views/auth/register-step2.blade.php
+++ resources/views/auth/register-step2.blade.php
@@ -6,7 +6,7 @@
 
         <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('register-step2.post') }}">
+        <form method="POST" action="{{ route('register-step2.post') }}" enctype="multipart/form-data">
             @csrf
 
             <div>
@@ -26,6 +26,11 @@
                         <option value="{{ $city->id }}">{{ $city->name }}</option>
                     @endforeach
                 </select>
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="photo" value="{{ __('Profile Photo') }}" />
+                <input type="file" name="photo" id="photo">
             </div>
 
             <div class="flex items-center justify-end mt-4">
