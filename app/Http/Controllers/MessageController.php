ce66b58eaefa58269ef22d031f53a3466a8503a1
Danny Avery <danny.w.avery@gmail.com>
2021-03-03 22:17:49 -0500

    Send email notifications for messages on listings

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use App\Models\Message;
+use App\Notifications\ListingMessageNotification;
 use Illuminate\Http\Request;
+// use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Notification;
 
 class MessageController extends Controller
 {
@@ -26,18 +30,29 @@ class MessageController extends Controller
     public function store(Request $request)
     {
         $request->validate(['message' => 'required']);
-        // save msg
+        $listing = Listing::with('user')->findOrFail($request->listing_id);
+        // check if there are more than 5 messages sent within the last minute
         $sentMessages = Message::where('user_id', auth()->id())
             ->where('created_at', '>', now()->subMinute())
             ->count();
         if ($sentMessages < 5) {
+            // save msg
             Message::create([
                 'user_id' => auth()->id(),
                 'listing_id' => request()->listing_id,
                 'message_text' => request()->message,
             ]);
+            // send msg
+            $message = [
+                'name' => auth()->user()->name,
+                'email' => auth()->user()->email,
+                'listingTitle' => $listing->title,
+                'messageText' => $request->message,
+            ];
+            Notification::route('mail', $listing->user->email)
+                ->notify(new ListingMessageNotification($message));
+            // TODO: set up mail in .env & test if mail sends
         }
         return redirect()->route('listings.index')->with('message', 'Message sent.');
-        // send msg
     }
 }
